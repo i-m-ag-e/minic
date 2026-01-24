aa34a6437bbc3f69e79b914f4f38d7d2bcc6803f
i-m-ag-e <akulgupta0207@gmail.com>
2026-01-25 04:20:35 +0530

    added compilation of generated assembly to object file or executable, added compiler driver options

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,10 @@ pub mod source_file;
 mod symbol;
 mod with_token;
 
-use anyhow;
+use std::{fs, path::Path, process::Command};
+
+use anyhow::{self, bail};
+use clap;
 use lexer::Lexer;
 use source_file::SourceFile;
 use symbol::SymbolTable;
@@ -18,9 +21,47 @@ use crate::{
     asm::AsmGen,
     lexer::{LexerResult, token::TokenType},
     parser::Parser,
+    source_file::SourcePosition,
 };
 
-pub fn compile(source_file: &SourceFile, only_lex: bool, only_parse: bool) -> anyhow::Result<()> {
+#[derive(clap::Parser)]
+#[command(name = "minic", version = "0.1.0")]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    /// Input source files
+    files: Vec<String>,
+
+    /// specify path to output file
+    #[arg(long, short)]
+    output: Option<String>,
+
+    /// generate object file
+    #[arg(short = 'c')]
+    object_file: bool,
+
+    /// only run the lexer
+    #[arg(long)]
+    lex: bool,
+
+    /// only run the lexer and parser
+    #[arg(long)]
+    parse: bool,
+
+    /// only run till the codegen phase
+    #[arg(long)]
+    codegen: bool,
+
+    /// only output assembly files
+    #[arg(short = 'S', long)]
+    only_assembly: bool,
+}
+
+pub fn assemble(
+    source_file: &SourceFile,
+    stop_at_lex: bool,
+    stop_at_parse: bool,
+    stop_at_codegen: bool,
+) -> anyhow::Result<String> {
     let mut interner = SymbolTable::new();
     let lexer = Lexer::new(source_file, &mut interner);
     let tokens: Vec<_> = lexer.collect::<LexerResult<Vec<_>>>()?;
@@ -40,7 +81,7 @@ pub fn compile(source_file: &SourceFile, only_lex: bool, only_parse: bool) -> an
         }
     }
 
-    let prog = if !only_lex {
+    let prog = if !stop_at_lex {
         let mut used_tokens = vec![false; tokens.len()];
         let mut parser = Parser::new(&tokens, &mut used_tokens);
 
@@ -48,15 +89,120 @@ pub fn compile(source_file: &SourceFile, only_lex: bool, only_parse: bool) -> an
         println!("{:#?}", prog);
         prog
     } else {
-        return Ok(());
+        return Ok(String::new());
     };
 
-    if !only_parse {
+    if !stop_at_parse {
         let mut asm_gen = AsmGen::new();
         let asm_program = asm_gen.generate(&prog);
         println!("{:?}", asm_program);
 
-        println!("{}", asm_program.to_string());
+        if !stop_at_codegen {
+            let asm_code = asm_program.to_string();
+            println!("{}", asm_code);
+            return Ok(asm_code);
+        }
     }
-    Ok(())
+
+    Ok(String::new())
+}
+
+fn make_error(
+    source_file: &SourceFile,
+    msg: &str,
+    span: (SourcePosition, SourcePosition),
+) -> anyhow::Error {
+    let line_col_start = source_file.line_col(span.0.0);
+    let line_col_end = source_file.line_col(span.1.0);
+    anyhow::anyhow!(
+        "{}",
+        format!(
+            "Error in file {} at {}:{} - {}:{} (at `{}`) :: {}",
+            source_file.filename(),
+            line_col_start.0,
+            line_col_start.1,
+            line_col_end.0,
+            line_col_end.1,
+            source_file[span.0.0..span.1.0].replace('\n', "\\n"),
+            msg
+        )
+    )
+}
+
+fn handle_compile_error(compile_err: anyhow::Error, source_file: &SourceFile) -> anyhow::Error {
+    let msg = compile_err.to_string();
+    let range = compile_err
+        .downcast_ref::<lexer_error::LexerError>()
+        .map(|e| e.range);
+
+    if let Some(lexer_err) = range {
+        make_error(source_file, &msg, lexer_err)
+    } else if let Some(parse_err) = compile_err.downcast_ref::<parser_error::ParserError>() {
+        make_error(source_file, &msg, parse_err.span)
+    } else {
+        compile_err
+    }
+}
+
+pub fn compile(cli: &Cli) -> anyhow::Result<()> {
+    // let asm = assemble(source_file, cli.lex, cli.parse, cli.codegen)?;
+
+    if cli.output.is_some() && cli.object_file && cli.files.len() > 1 {
+        bail!("Cannot specify output file when compiling multiple source files to object files",);
+    }
+
+    let asm_files = cli
+        .files
+        .iter()
+        .map(|file| {
+            Ok(Path::new(file)
+                .with_extension("s")
+                .file_name()
+                .ok_or(anyhow::anyhow!("invalid path: {}", file))?
+                .to_str()
+                .ok_or(anyhow::anyhow!("invalid characters in file path"))?
+                .to_owned())
+        })
+        .collect::<anyhow::Result<Vec<String>>>()?;
+
+    for (asm_file, source_file) in asm_files.iter().zip(&cli.files) {
+        let content = fs::read_to_string(source_file)
+            .map_err(|e| anyhow::anyhow!("Failed to read file `{}`: {}", source_file, e))?;
+        let source = SourceFile::new(source_file.clone(), content);
+        let asm = assemble(&source, cli.lex, cli.parse, cli.codegen)
+            .map_err(|e| handle_compile_error(e, &source))?;
+
+        if cli.lex || cli.parse || cli.codegen {
+            continue;
+        }
+
+        fs::write(asm_file, asm)
+            .map_err(|e| anyhow::anyhow!("Failed to write assembly file `{}`: {}", asm_file, e))?;
+    }
+
+    if cli.lex || cli.parse || cli.codegen || cli.only_assembly {
+        return Ok(());
+    }
+
+    let mut options = vec![];
+    if cli.object_file {
+        options.push("-c");
+    }
+
+    if let Some(ref output) = cli.output {
+        options.push("-o");
+        options.push(output);
+    }
+
+    let status = Command::new("gcc")
+        .args(&asm_files)
+        .args(&options)
+        .status()
+        .map_err(|e| anyhow::anyhow!("Failed to invoke gcc: {}", e))?;
+
+    if !status.success() {
+        bail!("gcc failed with exit code: {}", status);
+    } else {
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,14 @@
 use clap::Parser;
 use colored::Colorize;
-use minic::lexer_error::LexerError;
-use minic::parser_error::ParserError;
-use minic::source_file::SourceFile;
-use std::fs;
+use minic::Cli;
+use minic::compile;
 use std::process;
-
-#[derive(Parser)]
-#[command(name = "minic", version = "0.1.0")]
-#[command(version, about, long_about = None)]
-struct Cli {
-    /// Input source files
-    files: Vec<String>,
-
-    /// specify path to output file
-    #[arg(long, short)]
-    output: Option<String>,
-
-    /// generate object file
-    #[arg(short = 'c')]
-    object_file: bool,
-
-    /// only run the lexer
-    #[arg(long)]
-    lex: bool,
-
-    /// only run the lexer and parser
-    #[arg(long)]
-    parse: bool,
-
-    /// only run till the codegen phase
-    #[arg(long)]
-    codegen: bool,
-}
 
 fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
-    for file in cli.files {
-        let source = fs::read_to_string(&file)?;
-        let source_file = SourceFile::new(file.clone(), source);
-
-        let compile_result = minic::compile(&source_file, cli.lex, cli.parse);
-
-        let Ok(_) = compile_result else {
-            let compile_err = compile_result.unwrap_err();
-            let msg = compile_err.to_string();
-            let range = compile_err.downcast_ref::<LexerError>().map(|e| e.range);
-
-            if let Some(lexer_err) = range {
-                let line_col_start = source_file.line_col(lexer_err.0.0);
-                let line_col_end = source_file.line_col(lexer_err.1.0);
-                eprintln!(
-                    "{}",
-                    format!(
-                        "Error in file {} at {}:{} - {}:{} (at `{}`) :: {}",
-                        source_file.filename(),
-                        line_col_start.0,
-                        line_col_start.1,
-                        line_col_end.0,
-                        line_col_end.1,
-                        source_file[lexer_err.0.0..lexer_err.1.0].replace('\n', "\\n"),
-                        msg
-                    )
-                    .red()
-                );
-                process::exit(-1);
-            }
-
-            if let Some(parse_err) = compile_err.downcast_ref::<ParserError>() {
-                let line_col_start = source_file.line_col(parse_err.span.0.0);
-                let line_col_end = source_file.line_col(parse_err.span.1.0);
-                eprintln!(
-                    "{}",
-                    format!(
-                        "Error in file {} at {}:{} - {}:{} (at `{}`) :: {}",
-                        source_file.filename(),
-                        line_col_start.0,
-                        line_col_start.1,
-                        line_col_end.0,
-                        line_col_end.1,
-                        source_file[parse_err.span.0.0..parse_err.span.1.0].replace('\n', "\\n"),
-                        msg
-                    )
-                    .red()
-                );
-            }
-            process::exit(-1);
-        };
+    if let Err(e) = compile(&cli) {
+        eprintln!("{}", e.to_string().red());
+        process::exit(1);
     }
     Ok(())
 }
