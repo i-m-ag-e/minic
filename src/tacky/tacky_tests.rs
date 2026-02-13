ef593a44bcc13b1c9c590979fe87086d0de05e08
i-m-ag-e <akulgupta0207@gmail.com>
2026-02-13 18:08:48 +0530

    Updatesd tests to use the updated structures and added tests for logical and comparison operators

--- src/tacky/tacky_tests.rs
+++ src/tacky/tacky_tests.rs
@@ -10,15 +10,15 @@ use anyhow;
 use insta::assert_yaml_snapshot;
 
 fn test_string_success(s: &str) -> anyhow::Result<Program> {
-    let input = SourceFile::new("test_input.c".to_owned(), s.to_owned());
+    let mut input = SourceFile::new("test_input.c".to_owned(), s.to_owned());
     let mut symbol_table = SymbolTable::new();
     let lexer = Lexer::new(&input, &mut symbol_table);
-    let mut tokens = lexer.collect::<LexerResult<Vec<_>>>()?;
+    let tokens = lexer.collect::<LexerResult<Vec<_>>>()?;
     let mut used_tokens = vec![false; tokens.len()];
 
     let mut parser = Parser::new(&tokens, &mut used_tokens);
     let prog = parser.parse()?;
-    tokens = Parser::filter_saved_tokens(tokens, &mut used_tokens);
+    input.set_tokens(Parser::filter_saved_tokens(tokens, &mut used_tokens));
 
     let mut tacky_gen = TackyGen::new(&input);
     let tacky_prog = tacky_gen.visit_program(&prog);
@@ -40,5 +40,43 @@ fn test_unary_return() -> anyhow::Result<()> {
 #[test]
 fn test_group_unary() -> anyhow::Result<()> {
     assert_yaml_snapshot!(test_string_success("int main(void) { return -(-~-42); }")?);
+    Ok(())
+}
+
+#[test]
+fn test_binary() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(test_string_success("int main(void) { return 1 + 2 * 3; }")?);
+    Ok(())
+}
+
+#[test]
+fn test_logic() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(test_string_success(
+        "int main(void) { return 1 && 0 || 1; }"
+    )?);
+    Ok(())
+}
+
+#[test]
+fn test_logic_short_circuit() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(test_string_success(
+        "int main(void) { return 1 || (1 / 0); }"
+    )?);
+    Ok(())
+}
+
+#[test]
+fn test_comparison() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(test_string_success(
+        "int main(void) { return 1 < 2 && 3 >= 4; }"
+    )?);
+    Ok(())
+}
+
+#[test]
+fn test_comparison_equality() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(test_string_success(
+        "int main(void) { return 1 == 1 && 2 != 3; }"
+    )?);
     Ok(())
 }
